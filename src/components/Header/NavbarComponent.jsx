c4535d41a0891b5d08a077e50956543fdf51aa0e
Your Name <you@example.com>
2023-11-25 16:15:50 -0300

    arreglo de rutas

--- src/components/Header/NavbarComponent.jsx
+++ src/components/Header/NavbarComponent.jsx
@@ -6,24 +6,25 @@ import { Link } from 'react-router-dom';
 import logo from '../../img/logo.png';
 import '../../CSS/header.css'
 
-export const NavbarComponent = () => {
+export const 
+NavbarComponent = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Link to="/home">
+        <Link to="/PROYECTO-ECOMMERCE-ClasicsShoes/home">
           <img src={logo} alt="Logo de Classics Shoes" />
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-            <Nav.Link as={Link} to="/catalogo">Catalogo</Nav.Link>
-            <Nav.Link as={Link} to="/miPerfil">Mi Perfil</Nav.Link>
+            <Nav.Link as={Link} to="/PROYECTO-ECOMMERCE-ClasicsShoes/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/PROYECTO-ECOMMERCE-ClasicsShoes/catalogo">Catalogo</Nav.Link>
+            <Nav.Link as={Link} to="/PROYECTO-ECOMMERCE-ClasicsShoes/miPerfil">Mi Perfil</Nav.Link>
             <NavDropdown title="Inicia sesión/Registrate" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/login">
+              <NavDropdown.Item as={Link} to="/PROYECTO-ECOMMERCE-ClasicsShoes/login">
                 Inicia Sesión
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/signup">
+              <NavDropdown.Item as={Link} to="/PROYECTO-ECOMMERCE-ClasicsShoes/signup">
                 Registrate
               </NavDropdown.Item>
             </NavDropdown>
